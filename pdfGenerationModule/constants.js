8b589695be7ed85ee3e6dbe3267cf2ba60ca302d
Priyanka.Saha2 <priyanka.saha2@cognizant.com>
2018-11-04 21:37:58 +0530

    Get fixes

--- pdfGenerationModule/constants.js
+++ pdfGenerationModule/constants.js
@@ -7,7 +7,7 @@ module.exports = {
   SENDER_EMAIL_ID: 'priyanka.njoy9@gmail.com',
   SENDER_EMAIL_PASSWORD: 'butterfly4$',
   RECIEVER_EMAIL_ID: ['priyanka.saha2@cognizant.com', 'sahapri6@gmail.com'],
-  RECIEVER_COPY_EMAIL_ID: [],
+  RECIEVER_COPY_EMAIL_ID: ['tuhinr.007@gmail.com'],
   EMAIL_SUBJECT: 'Quotation created from Amazon ALEXA',
   EMAIL_BODY: 'Details of the Quotation created from Amazon ALEXA is attached'
 };
